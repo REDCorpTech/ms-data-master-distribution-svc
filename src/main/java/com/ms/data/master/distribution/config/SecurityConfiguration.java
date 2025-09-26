8fd27ef4e65d83a2f8aedcd0524e9887931ec1f7
nocturnalcoders <christiantolie99@gmail.com>
2025-09-26 13:17:55 +0800

    adding cors origin

---
+++ src/main/java/com/ms/data/master/distribution/config/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package com.ms.data.master.distribution.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOriginPattern("*://localhost:*"); // Allows all origins with "localhost" and any port
+        corsConfiguration.addAllowedOriginPattern("http://139.162.36.148:3000"); // Allows all origins with "localhost" and any port
+        corsConfiguration.addAllowedOriginPattern("http://139.162.36.148:3001"); // Allows all origins with "localhost" and any port
+        corsConfiguration.addAllowedOriginPattern("https://admin.ugkslimousine.com"); // Allows all origins with "ugkslimousine.com"
+        corsConfiguration.addAllowedOriginPattern("https://ugkslimousine.com"); // Allows all origins with "ugkslimousine.com"
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsFilter(source);
+    }
+}
